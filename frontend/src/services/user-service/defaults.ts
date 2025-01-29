8b96bc8a01f8fedb4dc2741729acecc5e238d78c
Henrik Sandström <henrik.sandstrom@sundsvall.se>
2025-01-29 14:55:05 +0100

    Default user value

--- frontend/src/services/user-service/defaults.ts
+++ frontend/src/services/user-service/defaults.ts
@@ -1,17 +1,18 @@
-import {
-  User,
-  //  Permissions
-} from '@interfaces/user';
+import { User, Permissions } from '@interfaces/user';
 import { ApiResponse } from '@services/api-service';
 
-// export const defaultPermissions: Permissions = {
-//     canEditSystemMessages: false,
-// };
+export const defaultPermissions: Permissions = {
+  canEditAdmin: false,
+  canViewAdmin: false,
+};
 
 export const emptyUser: User = {
+  firstName: '',
+  lastName: '',
   name: '',
   username: '',
-  //   permissions: defaultPermissions,
+  role: 'user',
+  permissions: defaultPermissions,
 };
 
 export const emptyUserResponse: ApiResponse<User> = {
